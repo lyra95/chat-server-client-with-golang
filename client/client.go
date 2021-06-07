b100751b4d19a10b60cc43d9b442754fef191cf5
jo tae hyouk <95hyouka@gmail.com>
2021-06-07 12:00:02 +0900

    first commit

---
+++ client/client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	//"time"
+)
+
+func main() {
+	//t, _ := time.ParseDuration("10m")
+	//conn, err := net.DialTimeout("tcp", "3.36.128.78:65507", t)
+	conn, err := net.Dial("tcp", "ec2-3-36-128-78.ap-northeast-2.compute.amazonaws.com:65507")
+	// conn, err := net.Dial("tcp", ":1000")
+	if err != nil {
+		fmt.Println("[Client] Can't connect to server")
+	}
+	defer conn.Close()
+	fmt.Println("[Client] Connected to server")
+	go ChatBoard(conn)
+	for {
+		var msg string
+		fmt.Scanln(&msg)
+		_, err := conn.Write([]byte(msg))
+		if err != nil {
+			fmt.Println("[Client] Connection lost.")
+			break
+		}
+	}
+	fmt.Println("[Client] exit")
+}
+
+func ChatBoard(conn net.Conn) {
+	readBuf := make([]byte, 4092)
+	for {
+		n, err := conn.Read(readBuf)
+		if err != nil {
+			log.Println("[Client] Connection lost")
+			break
+		}
+		msg := string(readBuf[:n])
+		if len(msg) > 0 {
+			log.Println(msg)
+		}
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Msg struct {
+	id  int
+	txt string
+}
+
+var ClientList map[int]net.Conn = make(map[int]net.Conn)
+var msgCh chan Msg = make(chan Msg, 100)
+
+func main() {
+	ln := StartServer("121.147.20.91:1000")
+	defer ln.Close()
+
+	go ChatBoard()
+
+	for id := 1; len(ClientList) < 100; id++ {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("[Server] Unknown Client failed to connect")
+			continue
+		}
+		go OnClientConnect(conn, id)
+	}
+	OnServerEnd()
+}
+func StartServer(endpoint string) net.Listener {
+	ln, err := net.Listen("tcp", endpoint)
+	if err != nil {
+		fmt.Println("[Server] Server Can't Start. Exit Program")
+		panic(err)
+	}
+	fmt.Println("[Server] Start tcp listening")
+	return ln
+}
+func OnServerEnd() {
+	SendAll("[Global] Server is going to be closed")
+}
+
+func SendAll(msg string) {
+	for id, client := range ClientList {
+		_, err := client.Write([]byte(msg))
+		if err != nil {
+			fmt.Printf("[Server] Client %v connection lost\n", id)
+			defer OnClientDisconneted(id)
+		}
+	}
+}
+
+func OnClientConnect(conn net.Conn, id int) {
+	defer conn.Close()
+	ClientList[id] = conn
+	fmt.Printf("[Server] Client %v is connected\n", id)
+	SendAll(fmt.Sprintf("[Global] Client %v is connected\n", id))
+	readBuf := make([]byte, 4092)
+	var msg string
+	for msg != "!exit" {
+		n, err := conn.Read(readBuf)
+		if err != nil {
+			fmt.Printf("[Server] Client %v connection lost\n", id)
+			OnClientDisconneted(id)
+			break
+		}
+		msg := string(readBuf[:n])
+		if len(msg) > 0 {
+			fmt.Printf("[Client %v] %s\n", id, msg)
+			msgCh <- Msg{id, msg}
+		}
+	}
+	fmt.Printf("[Server] Client %v exit\n", id)
+}
+
+func OnClientDisconneted(id int) {
+	delete(ClientList, id)
+	SendAll(fmt.Sprintf("[Global] Client %v disconnected", id))
+}
+
+func ChatBoard() {
+	for msg := range msgCh {
+		txt := fmt.Sprintf("[Client %v] %s\n", msg.id, msg.txt)
+		SendAll(txt)
+		/*
+			for id, client := range ClientList {
+				_, err := client.Write([]byte(txt))
+				if err != nil {
+					fmt.Printf("[Server] Client %v connection lost\n", id)
+					//delete(ClientList, id)
+					OnClientDisconneted(id)
+				}
+			}
+		*/
+	}
+}
