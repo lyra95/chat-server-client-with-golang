788e0faca7afa1970c84db8f0ce8a21bc929a33f
jo tae hyouk <95hyouka@gmail.com>
2021-06-07 13:17:08 +0900

    fixed std i/o operation.

--- client/client.go
+++ client/client.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	//"time"
 )
 
 func main() {
 	//t, _ := time.ParseDuration("10m")
 	//conn, err := net.DialTimeout("tcp", "3.36.128.78:65507", t)
-	conn, err := net.Dial("tcp", "ec2-3-36-128-78.ap-northeast-2.compute.amazonaws.com:65507")
-	// conn, err := net.Dial("tcp", ":1000")
+	// conn, err := net.Dial("tcp", "ec2-3-36-128-78.ap-northeast-2.compute.amazonaws.com:65507")
+	conn, err := net.Dial("tcp", ":1000")
 	if err != nil {
 		fmt.Println("[Client] Can't connect to server")
 	}
 	defer conn.Close()
 	fmt.Println("[Client] Connected to server")
 	go ChatBoard(conn)
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		var msg string
-		fmt.Scanln(&msg)
+		msg, _ := reader.ReadString('\n')
 		_, err := conn.Write([]byte(msg))
 		if err != nil {
 			fmt.Println("[Client] Connection lost.")
